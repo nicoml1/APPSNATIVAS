f3cb5e687b5d6d38e0ee13d214aeaf2ef11c7061
Jach00 <jjnyffe@gmail.com>
2022-11-15 18:49:05 -0300

    Update noticias.service.ts

--- noticiasya/src/app/inicio/noticias.service.ts
+++ noticiasya/src/app/inicio/noticias.service.ts
@@ -10,44 +10,44 @@ export class NoticiasService {
     {
       id: '1',
       title: 'Lionel Messi ya esta listo para el mundial de Qatar',
-      imageURL: 'https://th.bing.com/th/id/OIP.qrLDaqCWcoKmjFB3eHrnuAHaI6?pid=ImgDet&rs=1',
+      imageURL: 'https://cdn1.eldia.com/092022/1663987008313.jpg',
       subtitle: 'Deportes',
-      description: '14/11/2022: Lionel Messi ya está en Qatar y realizo su primer entrenamiento con la Seleccion Argentina, pensando en el partido amistoso contra los Emiratos Árabes de este Miércoles, y obviamente en el debut mundialista contra Arabia Saudita el 22/11. Argentina ya cuenta con sus 26 jugadores y todo su cuerpo técnico que van a competir en esta nueva copa del mundo'
+      description: '14/11/2022: Lionel Messi ya está en Qatar y realizo su primer entrenamiento con la Seleccion Argentina, pensando en el partido amistoso contra los Emiratos Árabes de este Miércoles, y obviamente en el debut mundialista contra Arabia Saudita el 22/11. Argentina ya cuenta con sus 26 jugadores y todo su cuerpo técnico que van a competir en esta nueva copa del mundo.'
     },
     {
       id: '2',
-      title: 'Título de la noticia',
+      title: 'La sostenibilidad del medio ambiente afectada por la población',
       imageURL: 'https://th.bing.com/th/id/OIP.qrLDaqCWcoKmjFB3eHrnuAHaI6?pid=ImgDet&rs=1',
-      subtitle: 'Categoría',
-      description: 'Descripción de la noticia. Descripción de la noticia. Descripción de la noticia.'
+      subtitle: 'Medio Ambiente',
+      description: '14/11/2022: A día de hoy, el gran aumento de la población afecta gravemente al medio ambiente. Según la ONU, en el mundo ya viven 8.000 millones de personas, y que para el 2100 se prevé que aumente en un 40%. Este incremento de la poblacion podría generar un fuerte impacto negativo sobre muchas especies, o también en varios ecosistemas.'
     },
     {
       id: '3',
-      title: 'Título de la noticia',
+      title: 'Problemas de salud para Alberto Fernández',
       imageURL: 'https://th.bing.com/th/id/OIP.qrLDaqCWcoKmjFB3eHrnuAHaI6?pid=ImgDet&rs=1',
-      subtitle: 'Categoría',
-      description: 'Descripción de la noticia. Descripción de la noticia. Descripción de la noticia.'
+      subtitle: 'Política',
+      description: '15/11/2022: El Presidente de la república Argentina se descompenso mientras estaba en la cumbre del G20, que se está realizando en Indonesia. Empezó teniendo mareos y nauseas, por ende se activó el protocolo de emergencia y fue llevado directamente al hospital de la ciudad de Bali. El diagnóstico dio que tuvo una gastritis erosiva, la cual le generó un sangrado interno. Sin embargo, Fernández ya se encuentra en buenas condiciones y pudo retomar sus actividades en el G20. Por ejemplo, se reunió con Xi Jiping, presidente de China.'
     },
     {
       id: '4',
-      title: 'Título de la noticia',
+      title: 'Nuevo Album de YSY A',
       imageURL: 'https://th.bing.com/th/id/OIP.qrLDaqCWcoKmjFB3eHrnuAHaI6?pid=ImgDet&rs=1',
-      subtitle: 'Categoría',
-      description: 'Descripción de la noticia. Descripción de la noticia. Descripción de la noticia.'
+      subtitle: 'Música',
+      description: '16/11/2022: El pasado 11 de Noviembre, el artista Argentino Alejo Nahuel Acosta o mejor conocido como "YSY A" lanzo su nuevo y cuarto album llamado "YSYSISMO", que tiene 12 canciones de estilos muy variados como Trap y Rap. La respuesta del publico hacia este disco fue muy positiva, siendo el album Argentino con más reproducciones en su primera semana desde su lkanzamiento, acumuluando 11 millones de reproducciones. Las canciones mas populares hasta ahora fueron "Todo Pago", "Reventando Relojes" y "Cuantos Terminos"'
     },
     {
       id: '5',
-      title: 'Título de la noticia',
+      title: 'Posible Ataque de Rusia a Polonia',
       imageURL: 'https://th.bing.com/th/id/OIP.qrLDaqCWcoKmjFB3eHrnuAHaI6?pid=ImgDet&rs=1',
-      subtitle: 'Categoría',
-      description: 'Descripción de la noticia. Descripción de la noticia. Descripción de la noticia.'
+      subtitle: 'Guerra',
+      description: '17/11/2022: En la madrugada del día de ayer, un misil ruso impacto en una zona rural de Polonia, dejanndo así 2 víctimas fatales. Esto fue muy cerca de la frontera entre Polonia y Ucrania (País que esta en guerra con Rusia). La situación creó mucha incertidumbre en el gobierno Polaco, por lo tanto, su Primer Ministro convoco a una reunión de urgencia, con su gabinete de seguridad para ver que medidad o decisones tomar al respecto, ya que Polonia es un país de la Otan.'
     },
     {
       id: '6',
-      title: 'Título de la noticia',
+      title: 'Pronostico desalentador de la economía Argentina en 2023',
       imageURL: 'https://th.bing.com/th/id/OIP.qrLDaqCWcoKmjFB3eHrnuAHaI6?pid=ImgDet&rs=1',
-      subtitle: 'Categoría',
-      description: 'Descripción de la noticia. Descripción de la noticia. Descripción de la noticia.'
+      subtitle: 'Economía',
+      description: '17/11/2022: Más de 50 consultoras argentinas y extranjeras pronosticaron una inflación mayor en un 90%. Los principales estudios detectaron una posible caída en el crecimiento economicó, riesgos para el pago de la deuda externa y mayor devaluación de la moneda. Con respecto a la divisa estadounidense, anticipan un valor de 318 pesos por dolar, además los impuestos aumentarían en un 35%.'
     },
   ]
 
