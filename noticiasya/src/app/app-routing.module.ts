13c8b720fa2a275a7c70174a7a7043d5aa621f40
nicoml1 <nmartinezlevrand@mail.austral.edu.ar>
2022-11-16 01:09:18 -0300

    login y registro

--- noticiasya/src/app/app-routing.module.ts
+++ noticiasya/src/app/app-routing.module.ts
@@ -4,7 +4,7 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path: '',
-    redirectTo: 'inicio',
+    redirectTo: 'login',
     pathMatch: 'full'
   },
   {
@@ -35,6 +35,14 @@ const routes: Routes = [
   {
     path: 'new-noticia',
     loadChildren: () => import('./inicio/addnoticia/addnoticia.module').then(m => m.AddnoticiaPageModule)
+  },
+  {
+    path: 'login',
+    loadChildren: () => import('./login/login.module').then( m => m.LoginPageModule)
+  },
+  {
+    path: 'registro',
+    loadChildren: () => import('./registro/registro.module').then( m => m.RegistroPageModule)
   }
 ];
 
