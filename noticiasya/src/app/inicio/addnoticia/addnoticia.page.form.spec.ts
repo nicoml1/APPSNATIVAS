797f6564befb850d044fb14405e7fb3e02b3ae95
Jach00 <jjnyffe@gmail.com>
2022-12-14 16:15:38 -0300

    reactive

--- noticiasya/src/app/inicio/addnoticia/addnoticia.page.form.spec.ts
+++ noticiasya/src/app/inicio/addnoticia/addnoticia.page.form.spec.ts
@@ -1,13 +1,55 @@
-import { FormBuilder } from "@angular/forms";
-import { AddnoticiaPageForm } from "./AddnoticiaPageForm";
+import { FormBuilder, FormGroup } from "@angular/forms";
+import { AddnoticiaPageForm } from "./addnoticia.page.form"
 
 describe ('AddnoticiaPageForm', ()=> {
 
-    it ('should create addnoticia form empty', ()=> {
-        const AddnoticiaPageForm = new AddnoticiaPageForm( new FormBuilder());
-        const form = AddnoticiaPageForm.createForm();
+    let addnoticiaPageForm: AddnoticiaPageForm;
+    let form: FormGroup
 
-       expect(form).not.toBeNull();
-       expect(form.get('Título')).not.toBeNull();
+    beforeEach(() => {
+        addnoticiaPageForm = new AddnoticiaPageForm(new FormBuilder());
+        form = addnoticiaPageForm.createForm();
     })
+   
+
+    it ('should create addnoticia form empty', ()=> {
+        
+       expect(form).not.toBeNull();
+       expect(form.get('titulo')).not.toBeNull();
+       expect(form.get('titulo').value).toEqual("");
+       expect(form.get('titulo').value).toBeFalsy();
+       expect(form.get('imagen')).not.toBeNull();
+       expect(form.get('imagen').value).toEqual("");
+       expect(form.get('imagen').value).toBeFalsy();
+       expect(form.get('categoria')).not.toBeNull();
+       expect(form.get('categoria').value).toEqual("");
+       expect(form.get('categoria').value).toBeFalsy();
+       expect(form.get('descripcion')).not.toBeNull();
+       expect(form.get('descripcion').value).toEqual("");
+       expect(form.get('descripcion').value).toBeFalsy()
+    })
+
+    it('should have imagen invalid if imagen is not valid', () => {
+        form.get('imagen').setValue('invalid imagen');
+        
+        expect(form.get('imagen').valid).toBeFalsy();
+    })
+
+    
+    it('should have imagen valid if imagen is valid', () => {
+        form.get('imagen').setValue('https://valid');
+
+        expect(form.get('imagen').valid).toBeTruthy();
+    })
+    
+    it('should have valid form', () => {
+        form.get('imagen').setValue('https://valid');
+        form.get('titulo').setValue('anytitulo')
+        form.get('categoria').setValue('anyCategoria');
+        form.get('descripcion').setValue('anyDescripcion');
+
+        expect(form.valid).toBeTruthy();
+    })
+
 })
+
